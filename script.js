e18fec3d79cd1ae29d90a3868887a4bf802f15e0
Oni <walidfikry5@gmail.com>
2024-07-11 22:34:04 +0200

    commit test aws

--- script.js
+++ script.js
@@ -788,7 +788,18 @@ window.onload = function () {
       })
   }
 
-
-
+      var richiesta = new Request("https://gztntkzeymn7dkk4lpruozitcm0jbzsz.lambda-url.us-east-1.on.aws/", {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json"
+        })
+      });
+    
+  
+    var response = fetch(richiesta).then(response => response.json())
+    response.then(res => {
+      console.log("response aws",res)
+      
+    })
 
 };
